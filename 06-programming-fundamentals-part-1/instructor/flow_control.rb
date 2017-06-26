a31592e3f7bedeaa83e5b02dfa24527a0d353a9d
Mina Mikhail <mina@fightingtheboss.com>
2017-06-26 15:17:40 -0400

    Add in-class code for 06 Programming Fundamentals Part 1

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -1,3 +1,22 @@
+number_of_cars = 110
+number_of_spaces = 50
+
+percentage_utilization = (number_of_cars.to_f / number_of_spaces) * 100.0
+
+if percentage_utilization > 95
+  puts "The lot is full"
+elsif percentage_utilization >= 50
+  puts "Surge pricing in effect"
+else
+  puts "The lot is currently #{percentage_utilization}% full."
+end
+
+surge_pricing = true if percentage_utilization >= 50
+
+
+puts "The lot is currently #{percentage_utilization}% full." unless surge_pricing
+
+
 # if statements
 
 # if (some condition is true)
@@ -7,14 +26,22 @@
 
 
 
+# case statement
+
+if number_of_cars == 10
+elsif number_of_cars == 20
+elsif number_of_cars == 30
+else
+end
 
 
-# Looping
-
-# for loop
-# while loop
-# until loop
-
-
-
-# Iteration
+case number_of_cars
+when 10, 110, 210
+  puts "tens"
+when 20
+  puts "twenties"
+when 30
+  puts "thirties"
+else
+  puts "else"
+end
---
+++ 06-programming-fundamentals-part-1/instructor/hello_world.rb
@@ -0,0 +1,3 @@
+puts "Hello Team Marvin!"
+
+puts "She said, \"That string isn't going to work!\""
