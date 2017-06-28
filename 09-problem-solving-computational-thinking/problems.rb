af8d91e1a689fdbb132fe6c23ae0c3495da762b5
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-28 16:26:38 -0400

    pre-class notes for 09-problem-solving-computational-thinking

---
+++ 09-problem-solving-computational-thinking/problems.rb
@@ -0,0 +1,8 @@
+# Problem 1: get the total sum of a list of numbers
+
+# Problem 2: find frequency of the letter "s" in a string
+
+# Problem 3: find frequency of the letter "a" in a string
+
+# Problem 4: find the most frequent letter in a string
+
