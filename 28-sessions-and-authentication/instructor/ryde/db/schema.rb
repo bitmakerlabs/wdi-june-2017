d17ba2705f31dc9428fe9744225e7e1d453fee00
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-07-27 12:13:51 -0400

    Add credits, and skeleton for logout link

--- 28-sessions-and-authentication/instructor/ryde/db/schema.rb
+++ 28-sessions-and-authentication/instructor/ryde/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727145321) do
+ActiveRecord::Schema.define(version: 20170727160945) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "credits"
   end
 
 end
