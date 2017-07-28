0aea1789916791661246f9078beea0ce6c653df0
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-07-28 10:26:21 -0400

    Params in GET requests

--- 29-params-security/instructor/ryde/app/controllers/rides_controller.rb
+++ 29-params-security/instructor/ryde/app/controllers/rides_controller.rb
@@ -1,4 +1,11 @@
 class RidesController < ApplicationController
   def index
+    Rails.logger.info "**** params[:controller] = #{ params[:controller] }"
+    Rails.logger.info "**** params[:action] = #{ params[:action] }"
+    Rails.logger.info "**** params[:lang] = #{ params[:lang] }"
+    Rails.logger.info "**** params[:country] = #{ params[:country] }"
+
+    @lang    = params[:lang]
+    @country = params[:country]
   end
 end
