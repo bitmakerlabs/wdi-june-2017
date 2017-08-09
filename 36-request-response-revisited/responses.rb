88ba5b9f1f16e22f422aa18ac0cf1de3e2f0e314
NatalieBlack <natalievictoriablack@gmail.com>
2017-08-09 00:44:42 -0400

    add gem to notes

--- 36-request-response-revisited/responses.rb
+++ 36-request-response-revisited/responses.rb
@@ -1,6 +1,9 @@
 # Standard library that makes HTTP requests from Ruby
 require 'net/http'
 
+# or use Typhoeus gem
+require 'typhoeus'
+
 # Standard library that parses string data into JSON format
 require 'json'
 
@@ -10,6 +13,8 @@ require 'csv'
 # Let's make some HTTP requests to a server and see what we get back
 # https://github.com/bitmakerlabs/response-types-demo
 # Net::HTTP.get('example.com', '/index.html')
+# Or we can use the gem
+#Typhoeus.get('example.com', '/index.html')
 
 #make get request to localhost:3000/monsters
 #result = Net::HTTP.get('localhost', '/monsters', 3000)
